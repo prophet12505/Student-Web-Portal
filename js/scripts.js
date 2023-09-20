91ef6be0945c7de916437f88007d22e294670704
1250523689@qq.com <1250523689@qq.com>
2023-09-20 12:16:36 -0500

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -6,39 +6,4 @@ var nameList=["KRINALBEN PATEL","RJ SUL",
 ];
 var desctiption=[];
 
-var showProfile=false;
-
-function collapseProfile(index) {
-    var pannelHover=$(".panel-hover");
-    var pannelHoverWrapper=$(".panel-hover-wrapper");
-    if(pannelHover.hasClass('open')) {
-        pannelHover.removeClass('open');
-        pannelHover.height(0);
-        $(".panel-hover").css("visibility", "hidden");
-    } else {
-        $(".panel-hover").css("visibility", "visible");
-        pannelHover.addClass('open');
-        pannelHover.height(pannelHoverWrapper.outerHeight(true));
-    }
-    $("#panel-profile-photo").attr("src", "./img/"+index+".png");
-};
-function setProfile(index){
-    if(!showProfile){
-        console.log('profile showed');
-        $('#panel-profile-name').attr('innerHTML',nameList[index-1])
-        //$(".panel-hover").css("visibility", "visible");
-        // $(".panel-hover").css("hight", "50vw");
-        $(".panel-hover").addClass("collapse");
-        showProfile=true;
-    }
-    else{
-        hideProfile();
-    }
-    
-    $("#panel-profile-photo").attr("src", "./img/"+index+".png");
-}
-function hideProfile(){
-    //$(".panel-hover").css("visibility", "hidden");
-    $(".panel-hover").removeClass("collapse");
-    showProfile=false;
-}
+$('.collapse-btn').on('click',function(){$('.collapse').collapse('hide');})
