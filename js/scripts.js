a3ade9bdd561e2b45939d7b3e775130396e1a897
1250523689@qq.com <1250523689@qq.com>
2023-09-20 09:12:40 -0500

    dealing with the flashing bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
+var nameList=[];
+var desctiption=[];
+
+var showProfile=false;
+
 
 function setProfile(index){
-    if($(".panel-hover").css("visibility") === "hidden")
-    {
+    if(!showProfile){
         $(".panel-hover").css("visibility", "visible");
+        showProfile=true;
     }
+    // if($(".panel-hover").css("visibility") === "hidden")
+    // {
+       
+    // }
     
     $("#panel-profile-photo").attr("src", "./img/"+index+".png");
 }
 function hideProfile(){
     $(".panel-hover").css("visibility", "hidden");
+    showProfile=false;
 }
